63e702a56a3f3a06ad5102af43eb437c8d618683
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-02-23 14:46:01 -0600

    Test

---
+++ Proyecto1/kotlinfirstapp/src/main/kotlin/com/kotlinfirstapp/kotlinfirstapp/KotlinfirstappApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinfirstapp.kotlinfirstapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinfirstappApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinfirstappApplication>(*args)
+}
---
+++ Proyecto1/kotlinfirstapp/src/test/kotlin/com/kotlinfirstapp/kotlinfirstapp/KotlinfirstappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinfirstapp.kotlinfirstapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinfirstappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
